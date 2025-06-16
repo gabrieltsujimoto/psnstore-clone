62ad56969735aef687c6a3851934f6db3a090cee
Gabriel macbook <gabrieltsujimotovicente@outlook.com>
2025-06-16 12:12:35 -0300

    adding dynamic images

--- src/app/components/menu-bar/menu-bar.component.ts
+++ src/app/components/menu-bar/menu-bar.component.ts
@@ -7,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrl: './menu-bar.component.css'
 })
 export class MenuBarComponent {
+  constructor(){
+    window.addEventListener('scroll',function (){
+      const navbar = document.getElementById('nav-wrapper');
+
+      if(window.scrollY > navbar!.scrollHeight.valueOf() ){
+        console.log('adding style')
+        navbar!.style.position = 'fixed' 
+
+        navbar!.style.top = '0px'
+        navbar!.style.width = '100%'
+        navbar!.style.backgroundColor = "white"
+        navbar!.style.transition = 'all 700ms'
+      } else if(window.scrollY < 80){
+         navbar!.style.position = 'relative'
+         navbar!.style.transition = 'all 700ms'
+      }
+    })
+  }
 
 }
--- src/app/pages/home/components/news/news.component.ts
+++ src/app/pages/home/components/news/news.component.ts
@@ -7,20 +7,27 @@ import { Component } from '@angular/core';
   styleUrl: './news.component.css'
 })
 export class NewsComponent {
-  protected imgUrl:string =''
-  constructor(){
-    document.getElementById('img1')?.addEventListener('click', (event) =>{
+  protected imgUrl: string = ''
+  constructor() {
+    window.addEventListener('scroll', () =>{
+      document.body.style.transition = 'colors 700ms'
+    })
+    document.getElementById('img1')?.addEventListener('click', (event) => {
       this.imgUrl = (event.target as HTMLImageElement).src;
       this.changeImgUrl(this.imgUrl);
     })
-
-    if(this.imgUrl === ""){
-      this.imgUrl = "https://gmedia.playstation.com/is/image/SIEPDC/COD-BLOPS-6-Season-4-Keyart-01-06jun25$pt?$1200px$"
+    if (this.imgUrl === "") {
+      this.imgUrl = "https://gmedia.playstation.com/is/image/SIEPDC/call-of-duty-black-ops-6-season-4-hero-desktop-01-en-15may25?$4000px$"
     }
   }
 
-  changeImgUrl(url: string): void{
+  changeImgUrl(url: string): void {
     this.imgUrl = url
+    if (this.imgUrl !== "https://gmedia.playstation.com/is/image/SIEPDC/call-of-duty-black-ops-6-season-4-hero-desktop-01-en-15may25?$4000px$") {
+      document.getElementById('img_banner__content')!.style.visibility = 'hidden'
+    } else {
+      document.getElementById('img_banner__content')!.style.visibility = 'visible'
+    }
     return console.log('url recebida:', url)
   }
 }
