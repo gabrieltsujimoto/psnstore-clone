b5e06b4be96e6afa2df0f449a5f768358dbf7874
gabrieltsujimoto <gabrieltsujimotovicente@outlook.com>
2025-06-16 16:15:43 -0300

    updating hardware news section

--- src/app/pages/home/components/news/news.component.ts
+++ src/app/pages/home/components/news/news.component.ts
@@ -10,7 +10,7 @@ export class NewsComponent {
   protected imgUrl: string = ''
   constructor() {
     window.addEventListener('scroll', () =>{
-      document.body.style.transition = 'colors 700ms'
+      document.body.style.transition = 'colors 400ms'
     })
     document.getElementById('img1')?.addEventListener('click', (event) => {
       this.imgUrl = (event.target as HTMLImageElement).src;
@@ -26,6 +26,7 @@ export class NewsComponent {
     if (this.imgUrl !== "https://gmedia.playstation.com/is/image/SIEPDC/call-of-duty-black-ops-6-season-4-hero-desktop-01-en-15may25?$4000px$") {
       document.getElementById('img_banner__content')!.style.visibility = 'hidden'
     } else {
+
       document.getElementById('img_banner__content')!.style.visibility = 'visible'
     }
     return console.log('url recebida:', url)
