364ef434b2be7b324e19594a1ea9029bc236bc37
gabrieltsujimoto <gabrieltsujimotovicente@outlook.com>
2025-06-15 23:57:53 -0300

    Trabalhando nas imagens

--- src/app/pages/home/components/news/news.component.ts
+++ src/app/pages/home/components/news/news.component.ts
@@ -7,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrl: './news.component.css'
 })
 export class NewsComponent {
+  protected imgUrl:string =''
+  constructor(){
+    document.getElementById('img1')?.addEventListener('click', (event) =>{
+      this.imgUrl = (event.target as HTMLImageElement).src;
+      this.changeImgUrl(this.imgUrl);
+    })
 
+    if(this.imgUrl === ""){
+      this.imgUrl = "https://gmedia.playstation.com/is/image/SIEPDC/COD-BLOPS-6-Season-4-Keyart-01-06jun25$pt?$1200px$"
+    }
+  }
+
+  changeImgUrl(url: string): void{
+    this.imgUrl = url
+    return console.log('url recebida:', url)
+  }
 }
